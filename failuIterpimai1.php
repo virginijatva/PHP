b98744d27bc11f9e7371402c6598e8beef5aa9a6
virginijatva <majauskaite.virginija@gmail.com>
2021-04-28 20:50:00 +0300

    Bank initial

--- failuIterpimai1.php
+++ failuIterpimai1.php
@@ -2,8 +2,15 @@
 
 define('DIR', __DIR__.'\\');
 
-include DIR.'\failuIterpimai2.php';
+include_once DIR.'\failuIterpimai2.php';
 
 echo 'A';
 
-include DIR.'\failuIterpimai2.php';
+include_once DIR.'\failuIterpimai2.php';
+
+function abra_kadabra(){
+    echo __FUNCTION__;
+    echo __FILE__;
+}
+
+abra_kadabra();
