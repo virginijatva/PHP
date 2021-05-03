46d3ffd77cf654ced118ed366eecc72ba03ad1ef
virginijatva <majauskaite.virginija@gmail.com>
2021-05-03 22:25:04 +0300

    Objektai: static, private

--- objektai/Bebras.php
+++ objektai/Bebras.php
@@ -1,6 +1,81 @@
 <?php
 
-class Bebras{
+class Bebras
+{
     public $color = 'brown';
-    private $tail = 'long';
+    private $tail = 45;
+
+    private static $dam = 'wooden';
+
+    //konstruktorius, kuriam galima kintamajam priskirti savybes reiksme by default
+    public function __construct($param)
+    {
+        echo '<br>Konstruktorius<br>';
+        if (is_string($param)) {
+            $this->defaultColor($param);
+        }
+        if (is_integer($param)) {
+            $this->defaultTail($param);
+        }
+    }
+
+    private function defaultColor(string $color)
+    {
+        echo '<br>Konstruktorius COLOR<br>';
+        $this->color = $color;
+    }
+
+    private function defaultTail(int $tail)
+    {
+        echo '<br>Konstruktorius TAIL<br>';
+        $this->tail = $tail;
+    }
+
+    // public function __get($prop){
+    //     echo '<br>Tokio dalyko kaip '.$prop.' Bebras neturi<br>';
+    // }
+
+    public function __get($prop)
+    {
+        if ('age' == $prop) {
+            return $this->BeaverAge();
+        }
+        //padaryti daaaaaaug tikrinimu
+        // return $this->$prop ?? 'nera ir nebus'; //reikia patikrinti, ar egzistuoja
+    }
+
+    public function __set($prop, $value)
+    {
+        //padaryti daaaaaaug tikrinimu
+        // $this->$prop = $value;
+    }
+
+    private function BeaverAge()
+    {
+        return rand(5, 25);
+    }
+
+    public function sayHello()
+    {
+        echo '<br> Hello, I am a beaver.';
+    }
+
+    public function getTail()
+    {
+        return $this->tail; //prie savybes tail ar kt. $ negali buti, nes ji nera kintamasis!
+    }
+
+    public function setTail($tail)
+    {
+        if ($tail != 'long' && $tail != 'super long') {
+            return;
+        }
+        $this->tail = $tail;
+    }
+
+    //kviecia save. self atstoja klases varda
+    public function callHome()
+    {
+        echo '<br>' . self::$dam . '<br>';
+    }
 }
--- objektai/index.php
+++ objektai/index.php
@@ -3,16 +3,38 @@
 require __DIR__ .'/Bebras.php';
 
 //objekto kurimas:
-$zveris1 = new Bebras; 
-$zveris2 = new Bebras();// galima rasyti ir be skliausteliu
+$zveris1 = new Bebras(12); // galima rasyti ir be skliausteliu
+$zveris2 = new Bebras('yellow');
 $zveris3 = $zveris1; 
 
+echo '<br>';
+echo '<br>';
+// echo Bebras::$dam;
+echo '<br>';
+
+$zveris1->callHome();
+$zveris2->callHome();
+
+
+echo '<br>';
+echo $zveris1->age;
+echo '<br>';
+
 //irasome klasei savybe:
-$zveris3->color = 'yellow';
+// $zveris3->color = 'black';
 $zveris3->age = 14;
+$zveris1->setTail('super long');
+
 //nuskaitom savybes, bet tik tas, kurios yra public:
 echo $zveris1->color;
+$zveris1->sayHello();
+
+$zveris1->tail = [];
+
 echo '<br>';
+echo 'My tail is '.$zveris1->tail.'<br>';
+echo 'My tail is '.$zveris1->getTail();
+echo '<br>';  
 
 var_dump($zveris1);
 echo '<br>';
